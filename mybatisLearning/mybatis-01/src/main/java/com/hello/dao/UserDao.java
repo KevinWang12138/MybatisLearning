669df255195583bbc809e6408994dd43bcb9af83
KevinWang12138 <wangqianming12138@gmail.com>
2022-02-03 15:52:22 +0800

    增删改查实现，map参数，模糊查询

--- mybatisLearning/mybatis-01/src/main/java/com/hello/dao/UserDao.java
+++ mybatisLearning/mybatis-01/src/main/java/com/hello/dao/UserDao.java
@@ -3,7 +3,15 @@ package com.hello.dao;
 import com.hello.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserDao {
-    List<User> getUserList();
+    List<User> getUserList();//查询全部用户
+    User getUserById(int id);//根据id查询用户
+    void addUser(User user);//插入用户
+    void update(User user);//修改用户
+    void deleteUser(int id);//删除用户
+    void addUser2(Map<String,Object> map);
+    User getUserById2(Map<String,Object> map);
+    List<User> getUserLike(String str);//模糊查询
 }
--- mybatisLearning/mybatis-01/src/test/java/com/hello/dao/UserDaoTest.java
+++ mybatisLearning/mybatis-01/src/test/java/com/hello/dao/UserDaoTest.java
@@ -5,16 +5,51 @@ import com.hello.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoTest {
     @Test
     public void test() {
         SqlSession sqlSession=MybatisUtils.getSqlSession();
         UserDao mapper=sqlSession.getMapper(UserDao.class);
-        List<User> userList=mapper.getUserList();
-        for(User user:userList){
-            System.out.println(user.getId()+user.getName()+user.getPwd());
+//        List<User> userList=mapper.getUserList();
+//        for(User user:userList){
+//            System.out.println(user.getId()+user.getName()+user.getPwd());
+//        }
+
+//        User u=mapper.getUserById(1);
+//        System.out.println(u.getName());
+
+//        //增删改需要提交事务
+//        mapper.addUser(new User(3,"王谦铭","20010821"));
+//        //提交事务
+//        sqlSession.commit();
+
+//        mapper.update(new User(3,"哈哈","123456"));
+//        sqlSession.commit();
+
+//        mapper.deleteUser(3);
+//        sqlSession.commit();
+
+//        Map<String,Object> map=new HashMap<String,Object>();
+//        map.put("userid",3);
+//        map.put("username","lalalala");
+//        map.put("userpassword","20010821");
+//        mapper.addUser2(map);
+//        sqlSession.commit();
+
+
+//        Map<String,Object> map=new HashMap<String,Object>();
+//        map.put("userid",1);
+//        User user=mapper.getUserById2(map);
+//        System.out.println(user.getName());
+
+        List<User> l=mapper.getUserLike("%wa%");//模糊查询必须传递通配符
+        //或者在sql中实现通配符
+        for(User u:l){
+            System.out.println(u.getName());
         }
     }
 }
